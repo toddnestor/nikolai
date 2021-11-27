58eb8e1be96d4d0683cf82b92a39ab0cfe55553c
Todd D Nestor <todd.nestor@gmail.com>
2021-11-27 10:53:05 -0800

    add history

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,9 @@ import {
   SliderTrack,
   SliderThumb,
   Slider,
-  SliderFilledTrack, AlertIcon, Alert,
+  SliderFilledTrack,
+  AlertIcon,
+  Alert,
 } from "@chakra-ui/react";
 import './App.css';
 
@@ -98,7 +100,27 @@ const humanCountTime = (duration: number): string => {
   return [str, humanCountTime(remainder)].join(' ');
 }
 
+function EstimatedTimeRemaining(props: { goal: number, start: number }) {
+  const [remaining, setRemaining] = React.useState(estimatedTimeRemaining(props.goal, props.start));
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setRemaining(estimatedTimeRemaining(props.goal, props.start));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <Box>
+    <Alert status="info" variant="solid">
+      <AlertIcon/>
+      There is an estimated {remaining} seconds remaining.
+    </Alert>
+  </Box>;
+}
+
 function App() {
+  const [history, setHistory] = React.useState<CountResult[]>([]);
   const [start, setStart] = React.useState<number | null>(null);
   const [goal, setGoal] = React.useState(1_000_000_000);
   const [loading, setLoading] = React.useState(false);
@@ -123,7 +145,10 @@ function App() {
         const json = await response.json() as CountResult;
         setStart(Date.now());
         setResults(json);
-        setLoading(!json.completed);
+        if (json.completed) {
+          setLoading(false);
+          setHistory([json, ...history]);
+        }
       } else {
         setError(true);
       }
@@ -139,6 +164,8 @@ function App() {
 
     let interval: NodeJS.Timeout | undefined;
 
+    const estimate = estimatedTime(results.goal);
+
     const poller = async () => {
       const response = await fetch(`https://api.nestopia.life/v1/get-count/${results.Id}`);
 
@@ -146,7 +173,10 @@ function App() {
         const json: CountResult = await response.json();
 
         setResults(json);
-        setLoading(!json.completed);
+        if (json.completed) {
+          setLoading(false);
+          setHistory([json, ...history]);
+        }
 
         if (json.completed && interval) {
           clearInterval(interval);
@@ -156,7 +186,11 @@ function App() {
       }
     };
 
-    interval = setInterval(() => poller(), 1000);
+    setTimeout(() => {
+      interval = setInterval(() => poller(), 1000);
+      // start polling  5 seconds before counting is estimated to be completed
+    }, Math.max(estimate - 5000, 0));
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [results?.Id]);
 
@@ -166,7 +200,7 @@ function App() {
         <VStack minWidth="350px">
           <h1>Nikolai's Counter</h1>
           <Box width="100%">
-            <NumberInput min={0} max={MAX} colorScheme="white" type="number" value={goal} onChange={(value) => setGoal(parseInt(value || '0'))}>
+            <NumberInput isDisabled={loading} min={0} max={MAX} colorScheme="white" type="number" value={goal} onChange={(value) => setGoal(parseInt(value || '0'))}>
               <NumberInputField />
               <NumberInputStepper>
                 <NumberIncrementStepper />
@@ -186,16 +220,11 @@ function App() {
           }
           {
             (!results || !results.completed) && start && (
-              <Box>
-                <Alert status="info" variant="solid">
-                  <AlertIcon />
-                  There is an estimated {estimatedTimeRemaining(goal, start)} seconds remaining.
-                </Alert>
-              </Box>
+              <EstimatedTimeRemaining goal={goal} start={start}/>
             )
           }
           <Box width="100%">
-            <Slider min={0} max={MAX} focusThumbOnChange={false} value={goal} onChange={value => setGoal(value)}>
+            <Slider isDisabled={loading} min={0} max={MAX} focusThumbOnChange={false} value={goal} onChange={value => setGoal(value)}>
               <SliderTrack>
                 <SliderFilledTrack />
               </SliderTrack>
@@ -203,7 +232,7 @@ function App() {
             </Slider>
           </Box>
           <Box width="100%">
-            <Button variant="outline" onClick={loadIt}>Count to {goal.toLocaleString()}!</Button>
+            <Button disabled={loading} variant="outline" onClick={loadIt}>Count to {goal.toLocaleString()}!</Button>
           </Box>
           <Box width="100%">
             {loading && <Spinner mt={4} size="xl"/>}
@@ -238,7 +267,48 @@ function App() {
             <br/>
             to count that high!
           </Box>
-          <Box pt={4}><sup>*</sup> assuming on average they count one number per second</Box>
+          <Box pt={4} pb={6}><sup>*</sup> assuming on average they count one number per second</Box>
+          {
+            history.length && (
+              <>
+                <Box width="100%" borderTop="2px solid white" pt={6}>
+                  <h2>History</h2>
+                </Box>
+                <Box width="100%">
+                  <StatGroup>
+                    <Stat>
+                      <StatLabel>Goal</StatLabel>
+                      <StatHelpText>
+                        What we counted to
+                      </StatHelpText>
+                    </Stat>
+
+                    <Stat>
+                      <StatLabel>Duration</StatLabel>
+                      <StatHelpText>
+                        How long it took
+                      </StatHelpText>
+                    </Stat>
+                  </StatGroup>
+                </Box>
+                {
+                  history.map((historicResult) => (
+                    <Box width="100%" borderTop="2px solid white" pt={2} pb={2}>
+                      <StatGroup>
+                        <Stat>
+                          <StatNumber>{historicResult.goal.toLocaleString()}</StatNumber>
+                        </Stat>
+
+                        <Stat>
+                          <StatNumber>{historicResult.duration}</StatNumber>
+                        </Stat>
+                      </StatGroup>
+                    </Box>
+                  ))
+                }
+              </>
+            )
+          }
         </VStack>
       </Box>
     </div>
